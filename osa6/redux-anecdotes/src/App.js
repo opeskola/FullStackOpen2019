cc823f785767259ed0e5fd3c21d40a0a17a8bad7
opeskola <opeskola@gmail.com>
2019-12-20 17:50:24 +0200

    6.4 done

--- osa6/redux-anecdotes/src/App.js
+++ osa6/redux-anecdotes/src/App.js
@@ -17,6 +17,21 @@ const App = (props) => {
     }
   }
 
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    props.store.dispatch({
+      type: 'NEW_ANECDOTE',
+      data: {
+        content,
+        votes: 0,
+        id: (100000 * Math.random()).toFixed(0)
+      }
+    })
+    event.target.anecdote.value = ''
+  }
+
+
   return (
     <div>
       <h2>Anecdotes</h2>
@@ -32,9 +47,9 @@ const App = (props) => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      <form onSubmit={addAnecdote}>
+        <div><input name="anecdote"/></div>
+        <button type="submit">create</button>
       </form>
     </div>
   )
--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -31,6 +31,8 @@ const reducer = (state = initialState, action) => {
       return state.map(anecdote =>
         anecdote.id !== id ? anecdote : votedAnecdote
       )
+    case 'NEW_ANECDOTE':
+      return [...state, action.data]
     default:
       return state
   }
